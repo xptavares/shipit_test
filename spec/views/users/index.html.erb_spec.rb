0255ac18d3ba3884b73492393efec91f46facf6e
Alexandre Tavares <xptavares@gmail.com>
2015-12-10 15:39:18 -0200

    add validate on user name

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "users/index", type: :view do
+  before(:each) do
+    assign(:users, [
+      User.create!(
+        :name => "Name",
+        :token => "Token"
+      ),
+      User.create!(
+        :name => "Name",
+        :token => "Token"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Token".to_s, :count => 2
+  end
+end
